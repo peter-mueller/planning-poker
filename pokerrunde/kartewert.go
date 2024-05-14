2f7599c5cb1bdc99e9450bb6d9f8d6b059791a78
XEMBIO <bauer.benedikt@posteo.de>
2024-05-14 13:13:37 +0000

    change default kartenwerte 84 to 89

--- pokerrunde/kartewert.go
+++ pokerrunde/kartewert.go
@@ -7,6 +7,6 @@ const KarteWertUnbekannt = KarteWert("")
 var DefaultKartenWerte = []KarteWert{
 	KarteWertUnbekannt,
 	"?", "0", "1", "2", "3", "5", "8",
-	"13", "21", "34", "55", "84",
+	"13", "21", "34", "55", "89",
 	"☕",
 }
